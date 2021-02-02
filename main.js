b088db87586bf925579180be23de166b235ae137
Yaathavi <yaathavi@gmail.com>
2021-02-02 01:06:59 -0500

    added ss of site

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let keys = ["KeyA", "KeyS", "KeyD", "KeyF","KeyG","KeyH","KeyJ","KeyW","KeyE", "KeyT", "KeyY", "KeyU"]
 
 document.addEventListener("keydown", function(event) {
-    //if key that was pressed (event.code) is included in the keys array do this
+    //if key that was pressed (event.code) is included in the keys array, do this
     if (keys.includes(event.code)) {
         let audio = new Audio(`${event.key}.mp3`);
         audio.play();
